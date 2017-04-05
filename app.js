4da57c64b2e52aa89e87521eb03c26307e222cf3
TonyCheng2016 <s110219004@stu.ntue.edu.tw>
2017-04-05 16:53:22 +0800

    mern-hw5

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/index.html', function(req, res) {
+	res.render('index');
+});
+
+app.get('/works.html', function(req, res) {
+	res.render('works');
+});
+
+app.get('/about.html', function(req, res) {
+	res.render('about');
+});
+
+app.get('/contact.html', function(req, res) {
+	res.render('contact');
+});
+
+app.get('/service.html', function(req, res) {
+	res.render('err');
+});
+
+app.get('*', function(req, res) {
+	res.render('err');
+});
+
+app.post('/home.html', urlencodedParser, function(req,res){
+	res.send("Thank you !");
+	console.log('Your firstname is ' + req.body.firstname);
+	console.log('Your lastname is ' + req.body.lastname);
+	console.log('Email is ' + req.body.email);
+	
+})
+
+app.listen(port);
